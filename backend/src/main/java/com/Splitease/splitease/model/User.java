fc4fca6bbf50f1d1293a961dda1dc97ca95986c4
Jiyakumari04 <verma.jiya2004jiya@gmail.com>
2026-07-06 13:49:03 +0530

    First commit

--- backend/src/main/java/com/Splitease/splitease/model/User.java
+++ backend/src/main/java/com/Splitease/splitease/model/User.java
@@ -1,4 +1,18 @@
 package com.Splitease.splitease.model;
 
-public class User {
-}
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+    @Entity
+    @Table(name="users")
+    @Data @NoArgsConstructor @AllArgsConstructor
+    public class User{
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+        @Column(unique = true)
+        private String email;
+        private String password;
+    }
+
