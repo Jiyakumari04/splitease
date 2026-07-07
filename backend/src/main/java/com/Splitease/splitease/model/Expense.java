107125a9ad0c35cd089b8e17c118ea20cba4a711
Jiyakumari04 <verma.jiya2004jiya@gmail.com>
2026-07-07 12:28:26 +0530

    Expenses

--- backend/src/main/java/com/Splitease/splitease/model/Expense.java
+++ backend/src/main/java/com/Splitease/splitease/model/Expense.java
@@ -1,4 +1,34 @@
 package com.Splitease.splitease.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Entity
+@Table(name="expenses")
+@Data
+@NoArgsConstructor @AllArgsConstructor
 public class Expense {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="group_id")
+    private Group group;
+
+    @ManyToOne
+    @JoinColumn(name="paid_by")
+    private User paidBy;
+
+    private BigDecimal amount;
+    private String description;
+    private String category;
+
+    @OneToMany(mappedBy = "expense", cascade= CascadeType.ALL)
+    private List<ExpenseSplit> splits;
 }
