94924c10af7e02ff92ef5a4cf41d033266c1921b
Jiyakumari04 <verma.jiya2004jiya@gmail.com>
2026-07-07 12:08:17 +0530

    Model: first commit

--- backend/src/main/java/com/Splitease/splitease/model/Group.java
+++ backend/src/main/java/com/Splitease/splitease/model/Group.java
@@ -1,4 +1,27 @@
 package com.Splitease.splitease.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="groups")
+@Data
+@NoArgsConstructor @AllArgsConstructor
 public class Group {
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name="created_by")
+    private User createdBy;
+    @ManyToOne
+    @JoinTable(name= "group_members",
+        joinColumns = @JoinColumn(name="group_id"),
+        inverseJoinColumns = @JoinColumn(name= "user_id"))
+    private Set<User> members= new HashSet<User>();
 }
