0607581989120956a86284e1c4a972a4d2a76509
Jiyakumari04 <verma.jiya2004jiya@gmail.com>
2026-07-07 12:33:35 +0530

    ExpenseSplit

--- backend/src/main/java/com/Splitease/splitease/model/ExpenseSplit.java
+++ backend/src/main/java/com/Splitease/splitease/model/ExpenseSplit.java
@@ -1,4 +1,29 @@
 package com.Splitease.splitease.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="expense_splits")
+@Data @NoArgsConstructor @AllArgsConstructor
 public class ExpenseSplit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="expense_id")
+    private Expense expense;
+
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    private BigDecimal amountOwed;
 }
+
+
